6fd250598049dcabb79d871cfb1136a5563f8c76
aktan <aktansuper2005@email.ru>
2022-02-13 17:20:57 +0600

    example

---
+++ example.py
@@ -0,0 +1,129 @@
+'''
+a = (17*3) 
+b = (12*5)
+c = a > b
+d = a < b
+e = a == b
+print('a болььше или b:',c)
+print('a меньше чем b:',d)
+print('a и b равны ли они:',e)
+
+a = 17925
+b = 34**2 > a
+c = 26*3 > a
+d = 17**33 > a
+e = 4394 > a
+print(d,c,b,e)
+'''
+'''
+a = 120
+b = 80
+c = 50
+d = 193432
+reshenie = (193432-(a + b * 5 ))
+print(reshenie)
+'''
+'''
+a = 25
+b = 20
+c = 30
+if a > b :
+	print('верно')
+if a > c :
+	print('не верно')
+if a > b :
+	print('верно')
+'''
+'''
+a = 7
+b = 3
+c = 4.8
+gaws = ((a % b)*4.8)
+print(gaws)
+'''
+'''
+vyrajenie1 = 455+200
+vyrajenie2 = 200+455
+asus = vyrajenie1 == vyrajenie2
+if vyrajenie1 == vyrajenie2:
+	print('все верно')
+'''
+'''
+gig = 12**7
+hih = 23**3
+if gig != hih:
+	print('все верно')
+'''
+'''
+ds = 2**3
+marvel = 3**2
+	print('неверно') 
+elif ds < marvel:
+	print('верно')
+'''
+'''
+a = int(input('введите '))
+if a % 2:
+	print('положительный число')
+
+if a % 3:
+	print('отрицательное число')
+'''
+'''
+a = range(0, 21)
+b = range(57, 100)
+numb = int(input('веди число'))
+if numb in a :
+	print('разрешенные числа')
+elif numb in b:
+	print('не разрешенные числа')
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+brawl = range ( 0, 51)
+stars = range (52 ,100)
+pam = int(input('введите букву'))
+if pam in brawl:
+	print('ращрешенные числа')
+elif pam in stars:
+	print('неразрешенные числа')
+'''
+a = 10//5
+b = 10/5
+if a == b:
+	print('равны')
+elif a != b:
+	print('не равны')
