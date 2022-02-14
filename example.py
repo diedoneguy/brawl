c0e0b2b9d9ad9768fed39487c987819a45e68ea8
aktan <aktansuper2005@email.ru>
2022-02-14 18:35:19 +0600

    example

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+
 '''
 a = (17*3) 
 b = (12*5)
@@ -118,8 +119,9 @@ stars = range (52 ,100)
 pam = int(input('введите букву'))
 if pam in brawl:
 	print('ращрешенные числа')
-elif pam in stars:
+-0_)=elif pam in stars:
 	print('неразрешенные числа')
+'''
 '''
 a = 10//5
 b = 10/5
@@ -127,3 +129,132 @@ if a == b:
 	print('равны')
 elif a != b:
 	print('не равны')
+'''
+'''
+dict = {'Вид компьютера':['ноутбук','ПК']}
+type = str(input(' вид компьтера:'))
+if type == 'ноутбук':
+	print ('хорошо')
+elif type == 'пк':
+	print('понял')
+else:
+	print('к сожалению у нас нет этого товара')
+ozu = str(input('размер памяти оперативки:'))
+if ozu > '4' :
+	print ('понял')
+dss = {'TYPE':['ssd','hdd']}
+print(dss)
+ssd  = str(input(' тип накопителя'))
+if ssd == 'ssd':
+	print ('хоршо')
+memore = int(input(' размер ' + str(ssd) ))
+if memore == 256:
+	print ('отличный выбор')
+elif memore == 500:
+	print('супермегабебра хорошш')
+cost = int(input('укажите сумму' + str(type)))
+if cost < 100000:
+	print('спасибо за покупку')
+elif cost == 100000:
+	print(' ваш ' + str(dict) + ' превысил сумму ')
+'''
+'''
+age = int(input('Укажите возраст'))
+if age > 18:
+	print(' Совершеннолетний ')
+elif age == 18:
+	print(' Совершеннолетний ')
+else:
+	print(' Несовршеннолетний ')
+'''
+'''
+a = {'a4 всем влад': ['влад', 'влад' ,'влад', 'привет', 'влад', 'с вами влад а четырее глент кобяков серега']}
+a.update({'opfg':['cfgopdjhotddhjothid']})
+print(a)
+'''
+'''
+menu = {'lagman': 120, 'plov': '120', 'borsh': 100}
+
+print(menu)
+
+menu.update({'besh_barmak': 130})
+
+print(menu)
+
+menu.update({'lagman' : 135})
+
+print(menu)
+
+menu.pop('borsh')
+
+print(menu)
+
+menu2 = {'Drinks':['Coca Cola','Fanta','Sprite']}
+
+print(menu2)
+
+menu2.update({'Natural juices' : ['orange']})
+
+print(menu2)
+
+menu2.pop('Drinks')
+
+print(menu2)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+cars = {'Машины':['мерседес'],'Дома'   :['особняк']}
+print (cars)
+cars.update({'Спорт Кары':['астон' 'мартин' 'масерати' 'бугатти']})
+print(cars)
+cars.update({'Собаки':['овчарки','доберманы','питбули']})
+print(cars)
+cars.update({'Каналы':['cartoon network''nickelodeon']})
+print (cars)
+cars.update({'Игры':['gta5','mafia2','cod']})
+print (cars)
+'''
+
+dict = {'name':['Bob'],'age':['25'],'Job':['Dev']}
