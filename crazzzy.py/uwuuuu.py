14a7cb71fd6f1bdce8f0b399d96386e7a1775d6a
aktan <aktansuper2005@email.ru>
2022-03-08 23:33:43 +0600

    realise vl1

---
+++ crazzzy.py/uwuuuu.py
@@ -0,0 +1,12 @@
+class cars:
+    mark = "lexus"
+    year = "2004"
+bmw = cars()
+bmw.mark = "bmw"
+bmw.year = "2004"
+print(bmw.mark,bmw.year)
+
+tesla = cars()
+tesla.mark = "tesla"
+tesla.year = "2201"
+print(tesla.mark,tesla.year)
